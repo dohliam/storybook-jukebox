dcaf02e1b940b91c6b6feea37b24a2d83b93e6e5
dohliam <dohliam@gmail.com>
2016-06-15 12:10:21 -0700

    refactor to fix issues on firefox

--- js/player.js
+++ js/player.js
@@ -8,25 +8,57 @@ function pad_num(n) {
   return num_string
 }
 
-function load_player(s) {
-  idx = window.stories[s].n;
-  text = window.stories[s].t;
-  trans_text = window.stories[s].b;
-  attribution = window.stories[s].a;
+function get_p(page, track) {
+  stories = window[window.json];
+  idx = stories[track].n;
+  text = stories[track].t;
+  trans_text = stories[track].b;
+  attribution = stories[track].a;
 
-  window.chapters = text.split("@");
-  window.trans_chapters = trans_text.split("@");
+  chapters = text.split("@");
+  trans_chapters = trans_text.split("@");
+  trans_chapter_text = trans_chapters[page];
 
-  title = window.chapters[0].replace(/#.*/, "");
+  len = chapters.length;
+
+  title = chapters[0].replace(/#.*/, "");
+  trans_title = trans_chapters[0];
+
+  page_number = pad_num(page + 1);
 
   chapter_text = "<center><strong>" + title + "</strong></center>";
-  page_number = pad_num(1);
-  p = [idx, title, page_number, chapter_text, attribution]
-  window.p = p;
-  create_player(p);
+  if (page != 0) {
+    chapter_text = chapters[page];
+  }
+  p = [idx, title, page_number, chapter_text, attribution, trans_title, trans_chapter_text, len];
+  return p;
 }
 
-function create_player(p) {
+
+function load_player(page, track) {
+  stories = window[window.json];
+  idx = stories[track].n;
+  text = stories[track].t;
+  trans_text = stories[track].b;
+  attribution = stories[track].a;
+
+  chapters = text.split("@");
+  trans_chapters = trans_text.split("@");
+  trans_chapter_text = trans_chapters[track];
+
+  len = chapters.length;
+
+  title = chapters[0].replace(/#.*/, "");
+  trans_title = trans_chapters[0];
+
+  chapter_text = "<center><strong>" + title + "</strong></center>";
+  page_number = pad_num(page + 1);
+  p = [idx, title, page_number, chapter_text, attribution, trans_title, trans_chapter_text, len];
+  create_player(p, page, track);
+  play_audio(page, track);
+}
+
+function create_player(p, page, track) {
   idx = p[0];
   title = p[1];
   page_number = p[2];
@@ -35,117 +67,139 @@ function create_player(p) {
 
   image = '<img src="https://raw.githubusercontent.com/global-asp/asp-imagebank/master/medium/' + idx + '/' + page_number + '.jpg" />\n\n';
 
-  title_format = title.toLowerCase().replace(/\s/g, "-").replace(/[\?\!,\(\)'؟？！]/g, "");
-  mp3 = 'https://raw.githubusercontent.com/global-asp/gasp-audio/master/' + lang + '/' + idx + '_' + title_format + '/mp3/' + page_number + '.mp3';
-  controls = "        <audio autoplay id='audio' onended='play_next_chapter()'><source src='" + mp3 + "' type='audio/mpeg'></audio>";
+  title_format = format_title(title);
+  audio_basename = 'https://raw.githubusercontent.com/global-asp/gasp-audio/master/' + lang + '/' + idx + '_' + title_format;
+  mp3 = audio_basename + '/mp3/' + page_number + '.mp3';
+  ogg = audio_basename + '/ogg/' + page_number + '.ogg';
+  audio_track = "        <audio autoplay id='audio' onended='play_next_chapter(" + page + "," + track + ")'><source src='" + ogg + "' type='audio/ogg'><source src='" + mp3 + "' type='audio/mpeg'></audio>";
 
   title_div = document.getElementById("title_div");
   image_div = document.getElementById("image_div");
   audio_div = document.getElementById("audio_item");
   text_div = document.getElementById("story_text");
 
-  show_lang();
+  sw = "switch_lang(" + page + "," + track + ")";
+
+  title_div.setAttribute("onclick", sw);
+  text_div.setAttribute("onclick", sw);
+
+  media_controls(page, track);
+  show_lang(p);
   image_div.innerHTML = image;
-  audio_div.innerHTML = controls;
-  media_controls();
+  audio_div.innerHTML = audio_track;
 }
 
-function play_next_chapter() {
-  p = window.p;
-  n = parseInt(p[2]);
+function play_audio(page, track) {
+  p = get_p(page, track);
+  lang = window.lang;
+  idx = p[0];
+  title = p[1];
+  page_number = p[2];
+
+  title_format = format_title(title);
+
+  audio_basename = 'https://raw.githubusercontent.com/global-asp/gasp-audio/master/' + lang + '/' + idx + '_' + title_format;
+  mp3 = audio_basename + '/mp3/' + page_number + '.mp3';
+  ogg = audio_basename + '/ogg/' + page_number + '.ogg';
+
+  audio_div = document.getElementById("audio_item");
+  controls = "        <audio autoplay id='audio' onended='play_next_chapter(" + page + "," + track + ")'><source src='" + ogg + "' type='audio/ogg'><source src='" + mp3 + "' type='audio/mpeg'></audio>";
+  audio_div.innerHTML = controls;
+}
+
+function format_title(title) {
+  title_format = title.toLowerCase().replace(/\s/g, "-").replace(/[\?\!,\(\)'؟？！]/g, "");
+  return title_format;
+}
+
+function play_next_chapter(page, track) {
+  p = get_p(page, track);
 
   idx = p[0];
   title = p[1];
   attribution = p[4];
+  len = p[7];
 
-  if (n + 1 > window.chapters.length) {
-    play_next();
+  page = page + 1;
+
+  if (page > len - 1) {
+    play_next_story(track);
   } else {
-    page_number = pad_num(n + 1);
-    chapter_text = window.chapters[n];
+    p = get_p(page, track);
+    chapter_text = p[3];
     if (chapter_text == "%None%") {
-      chapter_text = "";
-      p = [idx, title, page_number, chapter_text, attribution];
-      window.p = p;
-      setTimeout(create_player(p), 3000);
+      p[3] = "";
+      setTimeout(create_player(p, page, track), 3000);
     } else {
-      p = [idx, title, page_number, chapter_text, attribution];
-      window.p = p;
-      create_player(p);
+      create_player(p, page, track);
     }
   }
 }
 
-function play_previous_chapter() {
-  p = window.p;
-  n = parseInt(p[2]);
+function play_previous_chapter(page, track) {
+  p = get_p(page, track);
 
   idx = p[0];
   title = p[1];
   attribution = p[4];
 
-  if (n - 1 < 1) {
-    play_previous();
+  if (page - 1 < 0) {
+    play_previous_story(track);
   } else {
-    page_number = pad_num(n - 1);
-    chapter_text = window.chapters[n];
-    p = [idx, title, page_number, chapter_text, attribution];
-    window.p = p;
-    create_player(p);
+    page = page - 1;
+    p = get_p(page, track);
+    create_player(p, page, track);
   }
 }
 
-function play_next() {
-  s = window.current_track + 1;
-  if (s > window.stories.length - 1) {
-    s = 0;
+function play_next_story(track) {
+  stories = window[window.json];
+  track = track + 1;
+  if (track > stories.length - 1) {
+    track = 0;
   }
-  window.current_track = s;
-  load_player(s);
+  load_player(0, track);
 }
 
-function play_previous() {
-  s = window.current_track - 1;
-  if (s < 0) {
-    s = window.stories.length - 1;
+function play_previous_story(track) {
+  stories = window[window.json];
+  track = track - 1;
+  if (track < 0) {
+    track = window.stories.length - 1;
   }
-  window.current_track = s;
-  load_player(s);
+  load_player(0, track);
 }
 
-function switch_lang() {
+function switch_lang(page, track) {
+  p = get_p(page, track);
   h = window.switched;
   if (h) {
     window.switched = false;
   } else {
     window.switched = true;
   }
-  show_lang();
+  show_lang(p);
 }
 
-function show_lang() {
-  s = window.current_track;
-  title = window.chapters[0].replace(/#.*/, "");
+function show_lang(p) {
+  title = p[1];
+  page_number = p[2];
+  chapter_text = p[3];
+  trans_title = p[5];
+  trans_chapter_text = p[6];
 
   story_text = document.getElementById("story_text");
   title_div = document.getElementById("title_div");
 
-  trans_chapters = window.trans_chapters;
-
   trans_header = "        <h2>" + title + "</h2>\n\n";
-
-  p = window.p;
-
-  page_number = p[2];
-  chapter_text = p[3];
 
   n = parseInt(page_number) - 1;
 
   h = window.switched;
   if (h) {
-    header = "        <h2>" + trans_chapters[0] + "</h2>\n\n";
+    header = "        <h2>" + trans_title + "</h2>\n\n";
     title_div.innerHTML = header;
-    story_text.innerHTML = trans_chapters[n];
+    story_text.innerHTML = trans_chapter_text;
   } else {
     header = "        <h2>" + title.replace(/#/, "<br>") + "</h2>\n\n";
     title_div.innerHTML = header;
@@ -153,7 +207,8 @@ function show_lang() {
   }
 }
 
-function play_random() {
+function play_random(page, track) {
+  stories = window[window.json];
   rand = Math.floor(Math.random() * window.stories.length + 1);
 
   s = rand;
@@ -161,15 +216,15 @@ function play_random() {
     s = 0;
   }
   window.current_track = s;
-  load_player(s);
+  load_player(page, track);
 }
 
-function media_controls() {
-  audio_item = document.getElementById("audio_item");
+function media_controls(page, track) {
+  controls_div = document.getElementById("media_controls");
 
-  media_buttons = '    <div id="control_buttons">\n      <button onclick="play_previous()" accesskey="h"><img src="img/previous_story.svg" title="Listen to previous story"></button>\n      <button onclick="play_previous_chapter()" accesskey="j"><img src="img/previous_chapter.svg" title="Skip to previous chapter"></button>\n      <button onclick="play_pause()" accesskey="p"><img src="img/play_pause.svg" title="Pause/resume playback"></button>\n      <button onclick="audio_stop()" accesskey="x"><img src="img/stop.svg" title="Stop playback"></button>\n      <button onclick="play_next_chapter()" accesskey="k"><img src="img/next_chapter.svg" title="Skip to next chapter"></button>\n      <button onclick="play_next()" accesskey="l"><img src="img/next_story.svg" title="Listen to next story"></button>\n    </div>';
+  media_buttons = '    <div id="control_buttons">\n      <button onclick="play_previous_story(' + track + ')" accesskey="h"><img src="img/previous_story.svg" title="Listen to previous story"></button>\n      <button onclick="play_previous_chapter(' + page + ',' + track + ')" accesskey="j"><img src="img/previous_chapter.svg" title="Skip to previous chapter"></button>\n      <button onclick="play_pause()" accesskey="p"><img src="img/play_pause.svg" title="Pause/resume playback"></button>\n      <button onclick="audio_stop()" accesskey="x"><img src="img/stop.svg" title="Stop playback"></button>\n      <button onclick="play_next_chapter(' + page + ',' + track + ')" accesskey="k"><img src="img/next_chapter.svg" title="Skip to next chapter"></button>\n      <button onclick="play_next_story(' + track + ')" accesskey="l"><img src="img/next_story.svg" title="Listen to next story"></button>\n    </div>';
 
-  audio_item.innerHTML = audio_item.innerHTML + "\n" + media_buttons;
+  controls_div.innerHTML = media_buttons;
 
 }
 
@@ -201,7 +256,7 @@ function quick_api() {
   load_lang(lang);
 }
 
-function load_lang() {
+function load_lang(lang) {
   wl = 'js/json-' + lang + '.js';
   if (fileadded != wl) {
     var wlsrc = document.createElement('script');
